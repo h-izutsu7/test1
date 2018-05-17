c4cb2cf607e0cc9e57be24062c8b822960b0aa37
h-izutsu7 <39375027+h-izutsu7@users.noreply.github.com>
2018-05-18 05:29:30 +0900

    twitter

---
+++ twitter.php
@@ -0,0 +1,9 @@
+<?php
+
+$consumerKey = 'UhuBsj54DbRnnHGQYaPywHmAi';
+$consumerSecret = 'e1zSaGoQorVKzyW5W7U8LaAmkUsf3nnjdIHCxh45Y03bnnBj86';
+$accessToken = '42566916-5CI2BDtmiUQdWFKtTY6ajgxmLlYjURI4ftW2K88h3';
+$accessTokenSecret = 'YHfdFB1RJxIcHQ6XOriX0GZhmZmFpNmI5wegaBIRDflIU';
+$requestUrl = 'https://api.twitter.com/1.1/search/tweets.json';
+
+ ?>
